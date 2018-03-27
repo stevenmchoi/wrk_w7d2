f2d0fc76d19be3795131b5c637da49bb991bd6f2
Steven Choi <steven.m.choi@gmail.com>
2018-03-27 15:18:14 -0700

    Restart git to include rails project todos_app phase 2

---
+++ todos_app/db/migrate/20180327205015_create_todos.rb
@@ -0,0 +1,13 @@
+class CreateTodos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :todos do |t|
+      t.string :title, null: false
+      t.string :body, null: false
+      t.boolean :done
+
+      t.timestamps
+    end
+
+    add_index :todos, :title
+  end
+end
